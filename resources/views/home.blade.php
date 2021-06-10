9319d27902dd337c4458384f4b63a7e48a8a0978
danialessi <daniele.alessi@hotmail.it>
2021-06-10 18:02:34 +0200

    aggiunto un po' di stile (ci vorrebbero immagini)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset( 'css/app.css' )}}">
 </head>
 <body>
-    <h1>Elenco film</h1>
 
-    <ul>
-        @foreach ($movies as $movie)
-            <li>
+    <header>
+        <div class="title">
+            <h1>Fake Movie DB</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="movies-container">
+            @foreach($movies as $movie)
+            <div class="single-movie">
                 <div><strong>Titolo:</strong>{{ $movie['title']}}</div>
                 <div><strong>Titolo originale:</strong>{{ $movie['original_title']}}</div>
                 <div><strong>Data di uscita:</strong>{{ $movie['date']}}</div>
                 <div><strong>Voto:</strong>{{ $movie['vote']}}</div>
-            </li>
-        @endforeach
-    </ul>
+            </div>
+            @endforeach
+        </div>
+    </main>
+
+    
 </body>
 </html>
